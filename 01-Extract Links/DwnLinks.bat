42ab5a5d779bac66ad5f665dd93dd9f08c8c8833
taha zayed <tahazayed@gmail.com>
2017-04-19 15:46:44 +0200

    2017-04-19

---
+++ 01-Extract Links/DwnLinks.bat
@@ -0,0 +1,5 @@
+scrapy runspider CookpadSiteSpider.py
+
+cd ..\02-Extract Recipes
+
+DwnImpRecipes.bat
---
+++ 02-Extract Recipes/DwnImpRecipes.bat
@@ -0,0 +1,5 @@
+python Cookpad-c.py
+
+cd output
+
+for %%f in (*.json) do ("mongoimport.exe" -h taha-amin:27017 --jsonArray --db meals --collection "recipes" --file "%%~nf.json" /username:admin /password:dodido_2008 --authenticationDatabase admin)
